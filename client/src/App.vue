5b20080621a054e50f4fb4f4c90756eff92aa592
miladd3 <milad.d3@gmail.com>
2021-05-28 19:07:37 +0430

    🔎 Search Box and minor improvements

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   #app.todo-app
-    div {{ message }}
+    .todo-app__wrapper
+      SearchBox
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 
+import SearchBox from '@/components/SearchBox.vue';
+
 export default defineComponent({
   name: 'App',
+  components: { SearchBox },
   setup() {
     // utilise todo-bitpanda-server to get data
 
@@ -19,7 +23,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100,300;400&display=swap');
+@import "styles/global";
+
 .todo-app {
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: $color-bg;
+  height: 100vh;
+
+  .todo-app__wrapper {
+    max-width: $wrapper-width;
+    width: 100%;
+  }
 }
 </style>
---
+++ client/src/components/SearchBox.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  .search-box
+    input.search-box__input(placeholder="Search" v-model="valueLocal")
+    i.icon-search.search-box__icon
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'SearchBox',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+    const valueLocal = computed(() => props.value);
+
+    return { valueLocal };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.search-box {
+  position: relative;
+  align-items: center;
+  display: flex;
+
+  &__input {
+    background-color: $color-gray-1;
+    border: none;
+    height: rem-calc(32);
+    border-radius: rem-calc(12);
+    font-weight: 300;
+    padding: 0 rem-calc(16) 0 rem-calc(32);
+    outline: none;
+    width: 100%;
+
+    &:focus {
+      box-shadow: inset 0 0 4px $color-gray-5;
+    }
+  }
+
+  &__icon {
+    position: absolute;
+    color: $color-gray-8;
+    left: rem-calc(8);
+  }
+}
+</style>
